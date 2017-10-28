cc9e5841f69ef4eb20edaf1c9ad843ae28a7e003
Fibonacci- <helwigdev@gmail.com>
2017-10-28 15:41:56 -0400

    add testing route

--- rest.py
+++ rest.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello world!"
 
+@app.route('/apiecho')
+def apiecho():
+    return "the results of a sql query"
+
 if __name__ == '__main__':
     app.run(debug=True)
